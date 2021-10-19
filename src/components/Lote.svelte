059fbd18c643cf67aeffb2b7b3a84ef6324d4904
Matias Pando <matiaspando@gmail.com>
2021-10-19 13:54:26 -0300

    inicial

---
+++ src/components/Lote.svelte
@@ -0,0 +1,17 @@
+<script>
+    
+</script>
+
+<p class="lote"><slot>?</slot></p>
+
+<style>
+    .lote {
+        display: inline-block;
+        border-radius: 5px;
+        border: 2px solid  white;
+        padding: 4px;
+       font-weight: bolder;
+       width: 1.8em;
+       text-align: center;
+    }
+</style>
---
+++ src/pages/nueva_solicitud.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Page, Navbar, NavTitle } from "framework7-svelte";
+</script>
+
+<Page name="nueva_solicitud">
+    <Navbar backLink>
+        <NavTitle>Nueva solicitud</NavTitle>
+    </Navbar>
+</Page>
