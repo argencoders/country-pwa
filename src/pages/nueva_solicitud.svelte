7b300c7584b86399d04b136085947fd83af7eda5
junganscoder <jungansgamer@gmail.com>
2021-10-19 19:07:26 -0300

    reinicializado f7 con less

--- src/pages/nueva_solicitud.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { Page, Navbar, NavTitle } from "framework7-svelte";
-</script>
-
-<Page name="nueva_solicitud">
-    <Navbar backLink>
-        <NavTitle>Nueva solicitud</NavTitle>
-    </Navbar>
-</Page>
