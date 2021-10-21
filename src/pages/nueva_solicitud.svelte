a77520f434cddf248a9f3ef9a932db3f306ded3e
Matias Pando <matiaspando@gmail.com>
2021-10-21 20:59:49 -0300

    .

--- src/pages/nueva_solicitud.svelte
+++ src/pages/nueva_solicitud.svelte
@@ -1,22 +1,75 @@
 <script>
-    import { Page, Navbar, NavTitle, List, ListInput,Button } from "framework7-svelte";
+    import {
+        Page,
+        Navbar,
+        NavTitle,
+        List,
+        Button,
+        ListItem,
+        Icon,
+        Input,
+        ListInput,
+    } from "framework7-svelte";
+    let selected = "";
 </script>
 
 <Page name="nueva_solicitud">
     <Navbar backLink>
         <NavTitle>Nueva solicitud</NavTitle>
     </Navbar>
-    <List noHairlinesMd>
+    <List>
         <ListInput label="Lote" type="text" placeholder="lote" />
-        <ListInput label="Nombre" type="text" placeholder="nombre" />
-        <ListInput label="Motivo" type="select"  value="">
-            <i class="icon demo-list-icon" slot="left" />
-            <option value="Cadete">Cadete</option>
-            <option value="Plomero">Plomero</option>
-            <option value="Jardinero">Jardinero</option>
-            <option value="Electricista">Electricista</option>
-            <option value="Visita">Visita</option>
-        </ListInput>
+    </List>
+    <List menuList>
+        <ListItem
+            link
+            title="Cadete"
+            selected={selected === "cadete"}
+            onClick={() => (selected = "cadete")}
+        >
+            <span slot="media">
+                <Icon
+                    md="material:home"
+                    aurora="f7:house_fill"
+                    ios="f7:house_fill"
+                />
+            </span>
+        </ListItem>
+        <ListItem
+            link
+            title="Jardinero"
+            selected={selected === "jardinero"}
+            onClick={() => (selected = "jardinero")}
+        >
+            <span slot="media">
+                <Icon
+                    md="material:person"
+                    aurora="f7:person_fill"
+                    ios="f7:person_fill"
+                />
+            </span>
+        </ListItem>
+        <ListItem
+            link
+            title="Visita"
+            selected={selected === "visita"}
+            onClick={() => (selected = "visita")}
+        >
+            <span slot="media">
+                <Icon
+                    md="material:settings"
+                    aurora="f7:gear_alt_fill"
+                    ios="f7:gear_alt_fill"
+                />
+            </span>
+        </ListItem>
+    </List>
+    <List>
+        <ListInput
+            label="Notas"
+            type="textarea"
+            placeholder="nombre o descripcion o DNI"
+        />
     </List>
     <Button outline>Agregar</Button>
 </Page>
